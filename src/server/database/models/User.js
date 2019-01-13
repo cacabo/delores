8dd612838e9cacdfab47eba4d6f5c0e680d5280c
ccabo1 <ccabo@seas.upenn.edu>
2019-01-12 21:17:52 -0700

    Adding user model

---
+++ src/server/database/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  firstName: {
+    required: true,
+    type: String,
+  },
+  lastName: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+    unique: true,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
